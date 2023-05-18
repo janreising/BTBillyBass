bcd77023856d6a891d37b9faff140bb912d8f6e6
janreising <46487572+janreising@users.noreply.github.com>
2023-05-18 22:01:26 +0200

    Update BTBillyBass.ino
    it flaps :)

--- BTBillyBass.ino
+++ BTBillyBass.ino
@@ -29,25 +29,33 @@
 MX1508 bodyMotor(6, 9); // Sets up an MX1508 controlled motor on PWM pins 6 and 9
 MX1508 mouthMotor(5, 3); // Sets up an MX1508 controlled motor on PWM pins 5 and 3
 
-bool debug = false;
+bool debug = true;
 
 // SOUND INPUT
 int low_freq_pin = A1;
 int high_freq_pin = A2;
 
-int low_vol = 0;
-int high_vol = 0;
+int low_vol_read = 0;
+int high_vol_read = 0;
 
-int low_threshold = 10; // threshold for baseline
-int high_threshold = 10; // threshold for vocals
+float low_vol = 0;
+float high_vol = 0;
+
+int low_threshold = 2; // threshold for baseline
+int high_threshold = 100; // threshold for vocals
 
 // MOTOR VARIABLES
 int bodySpeed = 0; // body motor speed initialized to 0
 
 // ROLLING ARRAY VARIABLES
 
-// int counter = 0;
-// int arr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int counter = 0;
+//int low_arr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+// int high_arr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+int arr_size = 100;
+int low_arr[100] = {0};
+int high_arr[100] = {0};
 
 // FISH VARIABLES
 int fishState = 0; // variable to indicate the state Billy is in
@@ -109,8 +117,12 @@ void Head(){
 
     case 0: // Start and Wait
 
+      //Serial.println("vol: "+String(high_vol)+" thresh: "+String(high_threshold)+" "+String(high_vol > high_threshold));
+
       // threshold reached
       if (high_vol > high_threshold) {
+
+        //Serial.println("head state 1");
 
         headState = 1;
 
@@ -126,20 +138,24 @@ void Head(){
 
       } else {
 
+        //Serial.println("stopping");
         headState = 2; // move to stop talking
 
       }
     
-  case 2: // stop motion
+    case 2: // stop motion
 
-    yapping = false;
-    headState = 0; 
+      //Serial.println("head state 2");
+      yapping = false;
+      headState = 0; 
 
-  }
+    }
 
 }
 
 void Tail(){
+
+  //Serial.println("vol: "+String(low_vol)+" thresh: "+String(low_threshold)+" "+String(low_vol > low_threshold)+" "+String(flapping));
 
   switch (tailState) {
 
@@ -152,24 +168,30 @@ void Tail(){
 
       }
 
+      break;
+
     case 1: // flapping
 
       flapping = true;
-
+      
       if (low_vol > low_threshold) {
-
+        
         tailState = 1; // stay in flapping mode
 
       } else {
-
+        //Serial.println("flapping: "+String(flapping));
         tailState = 2; // move to stop flapping
 
       }
+
+      break;
     
     case 2: // stop motion
-
+      //Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stopping");
       flapping = false;
       tailState = 0; 
+
+      break;
 
   }
 
@@ -183,9 +205,14 @@ void moveMouth(){
 
       time_mouth_init = currentTime;
 
+      
+
       if (yapping){
 
-        mouthMotor.setSpeed(100);    
+        // Serial.println("start yapping");
+
+        mouthMotor.halt();
+        mouthMotor.setSpeed(250);    
         mouthMotor.forward();
 
         state_move_mouth = 1;
@@ -196,6 +223,8 @@ void moveMouth(){
 
       if (currentTime - time_mouth_init > 500){
         
+        // Serial.println("Stop yapping");
+
         mouthMotor.halt();    
         mouthMotor.backward();
 
@@ -225,107 +254,85 @@ void moveTail(){
 
       time_tail_init = currentTime;
 
-      if (yapping){
-
-        bodyMotor.setSpeed(100);    
-        bodyMotor.forward();
+      if (flapping){
+        Serial.println("start flapping");
+        
 
         state_move_tail = 1;
 
       }
 
+      break;
+
     case 1: // opening
 
-      if (currentTime - time_tail_init > 500){
-        
-        bodyMotor.halt();    
+      if (currentTime - time_tail_init > 100){
+        Serial.println("now we are here");
+        //bodyMotor.halt();    
+        //bodyMotor.backward();
+
+        bodyMotor.halt();        
+        bodyMotor.setSpeed(200);    
         bodyMotor.backward();
 
         state_move_tail = 2;
 
       }
 
+      break;
+
     case 2: // closing
 
-      if (currentTime - time_tail_init > 1000){
+      if (currentTime - time_tail_init > 500){
         
         bodyMotor.halt();    
         bodyMotor.setSpeed(0);
 
+        state_move_tail = 3;
+
+      }
+
+      break;
+
+      case 3: // waiting time
+
+      if (currentTime - time_tail_init > 2000){
+        
         state_move_tail = 0;
+        Serial.println("waiting over.");
 
       }
-  }
-}
 
-/*
-void SMBillyBass() {
-  switch (fishState) {
-    case 0: //START & WAITING
-      if (s2 > silence) { //if we detect audio input above the threshold
-        if (currentTime > mouthActionTime) { //and if we haven't yet scheduled a mouth movement
-          talking = true; //  set talking to true and schedule the mouth movement action
-          mouthActionTime = currentTime + 100;
-          fishState = 1; // jump to a talking state
-        }
-      } else if (currentTime > mouthActionTime + 100) { //if we're beyond the scheduled talking time, halt the motors
-        bodyMotor.halt();
-        mouthMotor.halt();
-      }
-      if (currentTime - lastActionTime > 1500) { //if Billy hasn't done anything in a while, we need to show he's bored
-        lastActionTime = currentTime + floor(random(30, 60)) * 1000L; //you can adjust the numbers here to change how often he flaps
-        fishState = 2; //jump to a flapping state!
-      }
-      break;
-
-    case 1: //TALKING
-      if (currentTime < mouthActionTime) { //if we have a scheduled mouthActionTime in the future....
-        if (talking) { // and if we think we should be talking
-          openMouth(); // then open the mouth and articulate the body
-          lastActionTime = currentTime;
-          articulateBody(true);
-        }
-      }
-      else { // otherwise, close the mouth, don't articulate the body, and set talking to false
-        closeMouth();
-        articulateBody(false);
-        talking = false;
-        fishState = 0; //jump back to waiting state
-      }
-      break;
-
-    case 2: //GOTTA FLAP!
-      //Serial.println("I'm bored. Gotta flap.");
-      flap();
-      fishState = 0;
       break;
   }
 }
-*/
+
 
 int updateSoundInput() {
 
   // read pins
-  low_vol = analogRead(low_freq_pin);
-  high_vol = analogRead(high_freq_pin);
+  low_vol_read = analogRead(low_freq_pin);
+  high_vol_read = analogRead(high_freq_pin);
 
   if (debug) {
 
-    Serial.print(low_vol);
-    Serial.print(",");
-    Serial.println(high_vol);
+    //Serial.print(low_vol);
+    //Serial.print(",");
+    //Serial.println(high_vol);
     
   }
 
   // rolling array
-  //if (counter > 20){
-  //  counter = 0;
-  //}
+  if (counter > arr_size){
+    counter = 0;
+  }
 
-  //arr[counter] = sV;
-  //counter = counter+1;
+  low_arr[counter] = low_vol_read;
+  high_arr[counter] = high_vol_read;
+  counter = counter+1;
 
-  //soundVolume = average(arr, 20);
+  low_vol = average(low_arr, arr_size);
+  high_vol = average(high_arr, arr_size);
 
 }
 
@@ -335,66 +342,3 @@ float average (int * array, int len) {  // assuming array is int.
     sum += array [i] ;
   return  ((float) sum) / len ;  // average will be fractional, so float may be appropriate.
 }
-
-/*
-void openMouth() {
-  mouthMotor.halt(); //stop the mouth motor
-  mouthMotor.setSpeed(220); //set the mouth motor speed
-  mouthMotor.forward(); //open the mouth
-}
-
-void closeMouth() {
-  mouthMotor.halt(); //stop the mouth motor
-  mouthMotor.setSpeed(180); //set the mouth motor speed
-  mouthMotor.backward(); // close the mouth
-}
-
-void articulateBody(bool talking) { //function for articulating the body
-  if (talking) { //if Billy is talking
-    if (currentTime > bodyActionTime) { // and if we don't have a scheduled body movement
-      int r = floor(random(0, 8)); // create a random number between 0 and 7)
-      if (r < 1) {
-        bodySpeed = 0; // don't move the body
-        bodyActionTime = currentTime + floor(random(500, 1000)); //schedule body action for .5 to 1 seconds from current time
-        bodyMotor.forward(); //move the body motor to raise the head
-
-      } else if (r < 3) {
-        bodySpeed = 150; //move the body slowly
-        bodyActionTime = currentTime + floor(random(500, 1000)); //schedule body action for .5 to 1 seconds from current time
-        bodyMotor.forward(); //move the body motor to raise the head
-
-      } else if (r == 4) {
-        bodySpeed = 200;  // move the body medium speed
-        bodyActionTime = currentTime + floor(random(500, 1000)); //schedule body action for .5 to 1 seconds from current time
-        bodyMotor.forward(); //move the body motor to raise the head
-
-      } else if ( r == 5 ) {
-        bodySpeed = 0; //set body motor speed to 0
-        bodyMotor.halt(); //stop the body motor (to keep from violent sudden direction changes)
-        bodyMotor.setSpeed(255); //set the body motor to full speed
-        bodyMotor.backward(); //move the body motor to raise the tail
-        bodyActionTime = currentTime + floor(random(900, 1200)); //schedule body action for .9 to 1.2 seconds from current time
-      }
-      else {
-        bodySpeed = 255; // move the body full speed
-        bodyMotor.forward(); //move the body motor to raise the head
-        bodyActionTime = currentTime + floor(random(1500, 3000)); //schedule action time for 1.5 to 3.0 seconds from current time
-      }
-    }
-
-    bodyMotor.setSpeed(bodySpeed); //set the body motor speed
-  } else {
-    if (currentTime > bodyActionTime) { //if we're beyond the scheduled body action time
-      bodyMotor.halt(); //stop the body motor
-      bodyActionTime = currentTime + floor(random(20, 50)); //set the next scheduled body action to current time plus .02 to .05 seconds
-    }
-  }
-}
-
-void flap() {
-  bodyMotor.setSpeed(180); //set the body motor to full speed
-  bodyMotor.backward(); //move the body motor to raise the tail
-  delay(500); //wait a bit, for dramatic effect
-  bodyMotor.halt(); //halt the motor
-}
-*/
